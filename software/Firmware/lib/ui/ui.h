2dcbe57e1fa8039c111d8a6ca7aae69791a3be15
Mathieu Donze <mathieu.donze@cern.ch>
2022-12-14 10:40:09 +0100

    WIP

---
+++ software/Firmware/lib/ui/ui.h
@@ -0,0 +1,43 @@
+// SquareLine LVGL GENERATED FILE
+// EDITOR VERSION: SquareLine Studio 1.1.1
+// LVGL VERSION: 8.2.0
+// PROJECT: heating_controller
+
+#ifndef _HEATING_CONTROLLER_UI_H
+#define _HEATING_CONTROLLER_UI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined __has_include
+#if __has_include("lvgl.h")
+#include "lvgl.h"
+#elif __has_include("lvgl/lvgl.h")
+#include "lvgl/lvgl.h"
+#else
+#include "lvgl.h"
+#endif
+#else
+#include "lvgl.h"
+#endif
+
+extern lv_obj_t * ui_Home_screen;
+extern lv_obj_t * ui_Temp_ext_panel;
+extern lv_obj_t * ui_Home_screen_Label1;
+extern lv_obj_t * ui_Temp_lim_panel;
+extern lv_obj_t * ui_Temp_lim;
+extern lv_obj_t * ui_Screen2;
+
+
+
+
+
+
+void ui_init(void);
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
---
+++ software/Firmware/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include "lv_conf.h"
+#include "lvgl.h"
+#include "ui.h"
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hello world!");
+
+
+}
+
+void loop() {
+  
+}
